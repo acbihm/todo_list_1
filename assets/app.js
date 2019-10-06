e393c7eea7921bef30dcc68d0a59aa0730e88bc6
Alex B <acbihm@gmail.com>
2019-10-06 11:31:00 -0500

    testing, mostly works

---
+++ assets/app.js
@@ -0,0 +1,40 @@
+
+    function renderToDos(list) {
+        $("#to-do-list").empty();
+        for (var i = 0; i < list.length; i++) {
+            var toDoItem = $("<p>");
+            toDoItem.text(list[i]);
+
+            var toDoClose = $("<button>");
+            toDoClose.attr("data-to-do", i);
+            toDoClose.addClass("checkbox");
+            toDoClose.text("x");
+
+            toDoItem = toDoItem.prepend(toDoClose);
+            $("#to-do-list").append(toDoItem);
+            console.log('h')
+
+        }
+    }
+
+    $("#add-to-do").on("click", function(event) {
+        event.preventDefault();
+        var toDoTask = $("#to-do").val().trim();
+        list.push(toDoTask);
+        renderToDos(list);
+        localStorage.setItem("todolist", JSON.stringify(list));
+        $("to-do").val("");
+    });
+
+    $(document).on("click", ".checkbox", function() {
+        var toDoNumber = $(this).attr("data-to-do");
+        list.splice(toDoNumber, 1);
+        renderToDos(list);
+        localStorage.setItem("todolist", JSON.stringify(list));
+    });
+    var list = JSON.parse(localStorage.getItem("todolist"));
+    if (!Array.isArray(list)) {
+        list = [];
+    }
+    
+    renderToDos(list);
